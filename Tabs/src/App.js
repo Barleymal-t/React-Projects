b930ec8314be65bb4b9639759df48115f0589515
Nii <niiodartey10@gmail.com>
2022-02-28 23:06:38 +0000

    beginning of the react tabs app

---
+++ Tabs/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+import {FaAngleDoubleRight} from 'react-icons/fa'
+
+const url = 'https://course-api.netlify.app/api/react-tabs-project'
+
+function App(){
+  const [loading,setLoading] = useState(true)
+  const [jobs,setJobs] =useState([])
+  const [value, setValue] = useState(0)
+
+
+  const fetchJobs = async ()=>{
+    const response = await fetch(url);
+    const newJobs = await response.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchJobs()
+  },[])
+  return <div>App</div>;
+};
+
+export default App;
