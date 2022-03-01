012d547a7636fb7c533286be8c016b96df0afb6b
Nii <niiodartey10@gmail.com>
2022-03-01 11:40:23 +0000

    tabs app completed

--- Tabs/src/App.js
+++ Tabs/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {FaAngleDoubleRight} from 'react-icons/fa'
 
-const url = 'https://course-api.netlify.app/api/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 
 function App(){
   const [loading,setLoading] = useState(true)
@@ -19,7 +19,45 @@ function App(){
   useEffect(()=>{
     fetchJobs()
   },[])
-  return <div>App</div>;
+
+  if (loading){
+  return <section className="section loading">
+    <h1>loading...</h1>
+  </section>;
+  }
+  const {company, dates, duties, title} =jobs[value]
+  return <section className="section">
+  <div className="title">
+  <h1>experience</h1>
+  </div>
+  <div className="jobs-center">
+    {/* {btn container} */}
+    <div className="btn-container">
+      {jobs.map((item,index)=>{
+        return (
+          <button className={`job-btn ${index ===value && 'active-btn'}`} key={index} onClick={() => setValue(index)}>
+            {item.company}
+          </button>
+        );
+      })}
+    </div>
+    {/* {job info} */}
+    <article className="job-info">
+    <h3>{title}</h3>
+    <h4>{company}</h4>
+    <p className="job-date">{dates}</p>
+    {duties.map((duty, index)=>{
+      return (
+        <div key={index} className="job-desc">
+            <FaAngleDoubleRight className="job-icon"/>
+            <p>{duty}</p>
+        </div>
+      )
+    })}
+
+    </article>
+  </div>
+  </section>
 };
 
 export default App;
